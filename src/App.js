473f9e1f192fd4c69715d1affb391c33b644c4fa
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-07-10 21:20:33 +0700

    pindahkan penangkap paramater login

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { listen } from './redux/listener';
 import { AppRoutes } from './routes';
+import { userLogin } from './redux/auth/actions';
+
 
 function App() {
   useEffect(() => {
     listen();
   }, []);
-  
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+    const email = urlParams.get('email');
+    const role = urlParams.get('role');
+    const refreshToken = urlParams.get('refreshToken');
+
+    console.log(token, role, refreshToken, email);
+
+    if (token && email && role && refreshToken) {
+      // Memanggil fungsi userLogin dengan parameter yang diterima
+      userLogin(token, role, refreshToken, email);
+    }
+  }, []);
   return (
     <>
       <BrowserRouter>
--- src/components/GuestOnlyRoute/index.js
+++ src/components/GuestOnlyRoute/index.js
@@ -1,25 +1,9 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
-import { userLogin } from '../../redux/auth/actions';
 
 
 export default function GuestOnlyRoute({ children }) {
-  React.useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get('token');
-    const email = urlParams.get('email');
-    const role = urlParams.get('role');
-    const refreshToken = urlParams.get('refreshToken');
-
-    console.log(token, role, refreshToken, email);
-
-    if (token && email && role && refreshToken) {
-      // Memanggil fungsi userLogin dengan parameter yang diterima
-      userLogin(token, role, refreshToken, email);
-    }
-  }, []);
-
   let { token } = useSelector((state) => state.auth);
 
   if (token) return <Navigate to="/" replace={true} />;
