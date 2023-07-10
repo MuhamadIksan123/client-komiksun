c705cb73b788382aa689926743832ade4d32337f
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-07-10 21:01:31 +0700

    update localStorige ke redux

--- src/App.js
+++ src/App.js
@@ -8,29 +8,7 @@ function App() {
   useEffect(() => {
     listen();
   }, []);
-
- useEffect(() => {
-   const urlParams = new URLSearchParams(window.location.search);
-   const token = urlParams.get('token');
-   const email = urlParams.get('email');
-   const role = urlParams.get('role');
-   const refreshToken = urlParams.get('refreshToken');
-
-   if (token && email && role && refreshToken) {
-     const authData = {
-       token,
-       email,
-       role,
-       refreshToken,
-     };
-
-     localStorage.setItem('auth', JSON.stringify(authData));
-   } else {
-     window.location.href = 'https://landingpage-komiksun.vercel.app/signin';
-   }
- }, []);
-
-
+  
   return (
     <>
       <BrowserRouter>
