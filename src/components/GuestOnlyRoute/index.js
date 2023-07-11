3d40ea13768efb43399290026967415afb3de123
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-07-11 09:35:39 +0700

    update redux 2

--- src/components/GuestOnlyRoute/index.js
+++ src/components/GuestOnlyRoute/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
-import { userLogin } from '../redux/auth/actions';
+import { userLogin } from '../../redux/auth/actions';
 
 export default function GuestOnlyRoute({ children }) {
   const dispatch = useDispatch();
