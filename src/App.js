e1045f4d568788ab6170655600840a4355450840
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-07-11 16:32:10 +0700

    console.log listener

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import { BrowserRouter, useSearchParams } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { listen } from './redux/listener';
 import { AppRoutes } from './routes';
 
 function App() {
-  // const [searchParams] = useSearchParams();
   useEffect(() => {
-    // const paramValue = searchParams.get('nama');
-    // console.log(paramValue);
-    // console.log('test app.js');
+    console.log(listen);
     listen();
   }, []);
   return (
